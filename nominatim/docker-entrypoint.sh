d9977504b06fb503a8cf934d60cc2d12fa847fac
Hung Dang <hung.dang980@gmail.com>
2019-09-30 00:39:53 +0700

    Add nominatim

---
+++ nominatim/docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Defaults
+NOMINATIM_DATA_PATH=${NOMINATIM_DATA_PATH:="/home/nominatim/data"}
+NOMINATIM_DATA_LABEL=${NOMINATIM_DATA_LABEL:="data"}
+NOMINATIM_PBF_URL=${NOMINATIM_PBF_URL:="http://download.geofabrik.de/asia/maldives-latest.osm.pbf"}
+NOMINATIM_POSTGRESQL_DATA_PATH=${NOMINATIM_POSTGRESQL_DATA_PATH:="/var/lib/postgresql/11/main"}
+# Google Storage variables
+NOMINATIM_GS_BUCKET=${NOMINATIM_GS_BUCKET:=""}
+
+# ========= Copy postgres data from S3 ========= 
+
+# Copy the archive from storage
+mkdir -p $NOMINATIM_DATA_PATH
+curl https://osm-nominatim.s3-ap-southeast-1.amazonaws.com/vn/nominatim_data.tgz_aa -o $NOMINATIM_DATA_PATH
+#gsutil -m cp $NOMINATIM_GS_BUCKET/$NOMINATIM_DATA_LABEL/*.tgz* $NOMINATIM_DATA_PATH
+
+# Remove any files present in the target directory
+rm -rf $NOMINATIM_POSTGRESQL_DATA_PATH/*
+
+# Extract the archive
+cat $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_* | tar xz -C $NOMINATIM_POSTGRESQL_DATA_PATH --strip-components=5
+# ========= Copy postgres data from S3 =========
+
+# ========== Start service =================
+stopServices() {
+        service apache2 stop
+        service postgresql stop
+}
+trap stopServices TERM
+
+service postgresql start
+service apache2 start
+
+# fork a process and wait for it
+tail -f /var/log/postgresql/postgresql-11-main.log &
+wait
