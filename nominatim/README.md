cb78b83b80f7af5a66d5f7b8b769d86bd7f5c57e
Hung Dang <hung.dang980@gmail.com>
2019-09-30 01:32:33 +0700

    fix docker-entry nominatim

---
+++ nominatim/README.md
@@ -0,0 +1,9 @@
+# Nominatim
+
+1. Đã tạo xong dockerfile up lên docker hub 
+2. Đã tạo xong data up lên s3
+3. Đã viết xong script download file S3 bỏ vào docker-entrypoint
+4. Giờ phải test
+docker run -p 7070:8080 --name nomimatim danghung/osm-nominatim
+
+docker run -p 7070:8080 --name nomimatim -v /Users/hung/projects/smartlog/openst/nominatim-data:/nominatim-data danghung/osm-nominatim
--- nominatim/docker-entrypoint.sh
+++ nominatim/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Defaults
-NOMINATIM_DATA_PATH=${NOMINATIM_DATA_PATH:="/home/nominatim/data"}
-NOMINATIM_DATA_LABEL=${NOMINATIM_DATA_LABEL:="data"}
+NOMINATIM_DATA_PATH=${NOMINATIM_DATA_PATH:="/nominatim-data"}
+NOMINATIM_DATA_LABEL=${NOMINATIM_DATA_LABEL:="nominatim_data"}
 NOMINATIM_PBF_URL=${NOMINATIM_PBF_URL:="http://download.geofabrik.de/asia/maldives-latest.osm.pbf"}
 NOMINATIM_POSTGRESQL_DATA_PATH=${NOMINATIM_POSTGRESQL_DATA_PATH:="/var/lib/postgresql/11/main"}
 # Google Storage variables
@@ -12,14 +12,19 @@ NOMINATIM_GS_BUCKET=${NOMINATIM_GS_BUCKET:=""}
 
 # Copy the archive from storage
 mkdir -p $NOMINATIM_DATA_PATH
-curl https://osm-nominatim.s3-ap-southeast-1.amazonaws.com/vn/nominatim_data.tgz_aa -o $NOMINATIM_DATA_PATH
+#curl https://osm-nominatim.s3-ap-southeast-1.amazonaws.com/vn/nominatim_data.tgz_aa -o $NOMINATIM_DATA_PATH/nominatim_data.tgz_aa
 #gsutil -m cp $NOMINATIM_GS_BUCKET/$NOMINATIM_DATA_LABEL/*.tgz* $NOMINATIM_DATA_PATH
 
 # Remove any files present in the target directory
 rm -rf $NOMINATIM_POSTGRESQL_DATA_PATH/*
 
 # Extract the archive
-cat $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_* | tar xz -C $NOMINATIM_POSTGRESQL_DATA_PATH --strip-components=5
+echo "bat dau gia nen $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_* $NOMINATIM_POSTGRESQL_DATA_PATH "
+#cat $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_* | tar xz -C $NOMINATIM_POSTGRESQL_DATA_PATH 
+tar -xvf $NOMINATIM_DATA_PATH/nominatim_data.tgz_aa -C $NOMINATIM_POSTGRESQL_DATA_PATH/
+mv $NOMINATIM_POSTGRESQL_DATA_PATH/postgresdata/* $NOMINATIM_POSTGRESQL_DATA_PATH/
+chown -R postgres:postgres $NOMINATIM_POSTGRESQL_DATA_PATH
+echo "ket thuc giai nen"
 # ========= Copy postgres data from S3 =========
 
 # ========== Start service =================
